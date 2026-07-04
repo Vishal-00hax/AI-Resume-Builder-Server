3c914c4005c35e277c6d1e2a0888386a33aac408
Vishal Panghal <vishalsinghpanghal@gmail.com>
2026-07-04 14:04:07 +0530

    Initial commit: server  application

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  res.status(200).json({ message: "Server is live" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at port: ${PORT}`);
+});
